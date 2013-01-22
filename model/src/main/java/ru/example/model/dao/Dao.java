f06ad26e5ca1e523695b28f09ed1200892922156
wolfhound <wolfhound@z-inc.net>
2013-01-22 21:12:44 +0600

    Dao

--- model/src/main/java/ru/example/model/dao/Dao.java
+++ model/src/main/java/ru/example/model/dao/Dao.java
@@ -1,5 +1,17 @@
 package ru.example.model.dao;
 
-public interface Dao {
+import ru.example.model.entity.Persistent;
 
+import java.util.List;
+
+public interface Dao<T extends Persistent> {
+    void saveOrUpdate(T persistent);
+
+    void delete(Long id);
+
+    void delete(T persistent);
+
+    T get(Long id);
+
+    List<T> getAll();
 }
---
+++ model/src/main/java/ru/example/model/dao/hibernate/AuthorHibernateDao.java
@@ -0,0 +1,40 @@
+package ru.example.model.dao.hibernate;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import ru.example.model.dao.Dao;
+import ru.example.model.entity.Author;
+
+import java.util.List;
+
+public class AuthorHibernateDao extends AbstractHibernateDao<Author> {
+    @Override
+    public void saveOrUpdate(Author persistent) {
+        getSession().save(persistent);
+    }
+
+    @Override
+    public void delete(Long id) {
+        Query query = getSession().createQuery("from Author where id=:id");
+        query.setLong("id", id);
+    }
+
+    @Override
+    public void delete(Author persistent) {
+        getSession().delete(persistent);
+    }
+
+    @Override
+    public Author get(Long id) {
+        return (Author) getSession().get(Author.class, id);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Author> getAll() {
+        return getSession().createQuery("from Author").list();
+    }
+
+
+}
